bb6b6c7b403b7d18b72d91b3670b7d06efde2fd8
spyrost <spyrostsipas7@gmail.com>
2024-04-09 11:35:19 +0200

    play_audio

--- app/src/main/java/com/example/dictionayapplication/ToDo.kt
+++ app/src/main/java/com/example/dictionayapplication/ToDo.kt
@@ -1,12 +1,11 @@
 package com.example.dictionayapplication
-
 /**
  * For this project:
- * I want to fix the play button so i can hear the word,
- * I want to hide the box before the searched word.
- * I want to add a snackbar when there is no word.
- * I want also add a border in the top side of the box,
- * I want to change the color of the app .
+ * I want to fix the play button so i can hear the word,DONE v
+ * I want to hide the box before the searched word.v
+ * I want to add a snackbar when there is no word. v
+ * I want also add a border in the top side of the box -
+ * I want to change the color of the app.
  *
  * @author
  * Features: I want to add camera recognition to find the word!
@@ -14,3 +13,4 @@ package com.example.dictionayapplication
  *
  *
  **/
+
